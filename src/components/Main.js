6015fd9942338747aa9ce676c2cb1e041eacae28
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-10-05 09:22:52 -0600

    added favorite, needs work, and cleaned up code

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,55 +1,62 @@
-import {Component} from "react";
-import {withAuth0} from '@auth0/auth0-react';
+import { Component } from "react";
+import { withAuth0 } from "@auth0/auth0-react";
 import Recipecard from "./RecipeCard";
-import RecipeModal from './RecipeModal';
-import Search from './Search';
-import Favorites from './Favorites';
+import RecipeModal from "./RecipeModal";
+import Search from "./Search";
+// import FavoritesList from "./Favorites";
 import axios from "axios";
-
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showRecipeModal: false,
-      searchQuery: '',
-      favorite: false
-    }
+      searchQuery: "",
+      favorites: [],
+      searchButton:	"🔍",
+    };
   }
 
   handleModal = () => {
     this.setState({
-      showRecipeModal: !this.state.showRecipeModal
-      
-    })
-    
-  }
+      showRecipeModal: !this.state.showRecipeModal,
+    });
+  };
 
   getRecipes = async (event) => {
     event.preventDefault();
-    let recipeURL=`${process.env.REACT_APP_SERVER}/recipe`
+    let recipeURL = `${process.env.REACT_APP_SERVER}/recipe`;
     const recipeResponse = await axios.get(recipeURL);
-        console.log('get recipes is working')
+    console.log("get recipes is working");
+  };
 
-
-  }
   getSearchQuery = (userSearchQuery) => {
     this.setState({
-      searchQuery: userSearchQuery
-    })
-    console.log('get search query is working')
-  }
+      searchQuery: userSearchQuery,
+    });
+    console.log("get search query is working");
+  };
+
+  addFavorite = (favorite) => {
+    const { favorites } = this.state;
+    if (
+      !favorites.some((alreadyFavorite) => alreadyFavorite.id === favorite.id)
+    ) {
+      this.setState({
+        favorites: [...this.state.favorites, favorite],
+      });
+    }
+  };
 
   render() {
-    console.log(this.state.searchQuery)
+    console.log(this.state.searchQuery);
     return (
       <>
-
-       <Favorites show={this.state.favorite}/>       
-       <Search searchQuery={this.state.searchQuery} getSearchQuery={this.getSearchQuery} getRecipes={this.getRecipes}/>
-       <Recipecard handleModal={this.handleModal}/>
-       <RecipeModal show={this.state.showRecipeModal} handleModal={this.handleModal}/>
-      
+        {/* <FavoritesList favorites={this.state.favorites} /> */}
+        <Search searchQuery={this.state.searchQuery} getSearchQuery={this.getSearchQuery} getRecipes={this.getRecipes}/>
+        <Recipecard handleModal={this.handleModal} />
+        <RecipeModal show={this.state.showRecipeModal} handleModal={this.handleModal}
+        />
       </>
     );
   }
